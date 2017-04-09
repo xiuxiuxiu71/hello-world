c3d8f412450cb526576d530b4409b518b59f8ef4
xiuxiuxiu71 <1292071478@qq.com>
2017-04-09 16:38:41 +0800

    hw0_data.dat处理，选定一列并进行排序。（如果我没有理解错的话。。。）

---
+++ hw01.py
@@ -0,0 +1,28 @@
+
+filename = "hw0_data.dat";
+fl = open(filename,"r");
+ls = [];
+mid = [];
+positive = [];
+negative = [];
+result = [];
+choice = input("please select a column to sort:");
+if choice == 0:
+    print("index error");
+    exit();
+try:
+    for line in fl:
+        ls = line.split(" ");
+        mid.append(float(ls[choice-1]));
+    for ele in mid:
+        if ele > 0:
+            positive.append(ele);
+        else:
+            negative.append(ele);
+    negative.sort();
+    positive.sort();
+    result = negative + positive;
+    print(result);
+except IndexError:
+    print("index error");
+
